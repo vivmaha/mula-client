b937bf47d874c14937039461d587cd08629e1304
V Maharajh <vmaharajh@tableau.com>
2020-01-19 18:17:42 -0800

    Added Prettier as a formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import './App.css';
-import { CssBaseline, AppBar, Toolbar, Typography, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import "./App.css";
+import {
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
   title: {
     flexGrow: 1,
-    textAlign: "left",
-  },
+    textAlign: "left"
+  }
 }));
 
 const App: React.FC = () => {
@@ -18,12 +24,14 @@ const App: React.FC = () => {
       <CssBaseline />
       <AppBar>
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>Home</Typography>
+          <Typography variant="h6" className={classes.title}>
+            Home
+          </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
   );
-}
+};
 
 export default App;
